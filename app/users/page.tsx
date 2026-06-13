69087ecc350cdf78612764c6cc61b07b6e98ca8f
Mehdi Moradzadeh <morad97mm@gmail.com>
2026-06-13 18:19:42 -0400

    initial defualt setup of my-company-manager

---
+++ app/users/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+            <div className='pl-2'>page</div>
+            <button className='btn btn-danger'>Click me</button>
+        </>
+    )
+}
+
+export default page
